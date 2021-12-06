b836a96944a29bddb735326f2122a0b1f33443b8
sp863 <sp863429@gmail.com>
2021-12-07 02:47:24 +0900

    js02-29 : conditional operator

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -284,3 +284,59 @@ else {
   console.log("Nobody wins any trophies");
 }
 */
+
+/*
+const day = "monday";
+
+switch (day) {
+  case "monday": // day === 'monday'
+    console.log("Plan course structure");
+    console.log("Go to coding meetup");
+    break;
+  case "tuesday":
+    console.log("Prepare theory videos");
+    break;
+  case "wednesday":
+  case "thursday":
+    console.log("Write code examples");
+    break;
+  case "friday":
+    console.log("Record videos");
+    break;
+  case "saturday":
+  case "sunday":
+    console.log("enjoy the weekend");
+    break;
+  default:
+    console.log("Not a valid day");
+}
+*/
+
+/*
+const age = 23;
+age >= 18
+  ? console.log("I like to drink wine")
+  : console.log("I like to drink water");
+
+const drink = age >= 18 ? "wine" : "water";
+console.log(drink);
+
+let drink2;
+if (age >= 18) {
+  drink2 = "wine";
+} else {
+  drink2 = "water";
+}
+console.log(drink2);
+
+console.log(`I like to drink ${age >= 18 ? "wine" : "water"}`);
+*/
+
+const bill = 275;
+let tip;
+
+console.log(
+  `The bill was ${bill}, the tip was ${
+    bill <= 300 && bill >= 50 ? (tip = bill * 0.15) : (tip = bill * 0.2)
+  } and the total value is ${bill + tip}`
+);
