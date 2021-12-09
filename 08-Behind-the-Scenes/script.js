42b52565ebe1d39b5af4a81272e42a073c830643
sp863 <sp863429@gmail.com>
2021-12-09 21:15:25 +0900

    js08-95 : hoisting

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -1,5 +1,5 @@
 "use strict";
-
+/*
 function calcAge(birthYear) {
   const age = 2037 - birthYear;
 
@@ -31,3 +31,48 @@ const firstName = "Jonas";
 calcAge(1991);
 
 // console.log(age);
+*/
+
+//Variables
+// console.log(me);
+// console.log(job);
+// console.log(year);
+
+// var me = "Jonas";
+// let job = "teacher";
+// const year = 1991;
+
+//functions
+/*
+console.log(addDecl(2, 3));
+console.log(addExpr(2, 3));
+console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+// Example
+//numProducts here is undefined because it hasn't been declared yet
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log("All products deleted!");
+}
+*/
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
