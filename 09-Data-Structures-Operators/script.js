debc6bdd9f37eff392a64f85f15a6073580b681f
sp863 <sp863429@gmail.com>
2021-12-10 20:41:30 +0900

    js09-114 : looping objects

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -54,6 +54,30 @@ const restaurant = {
   },
 };
 
+//Property names
+const properties = Object.keys(openingHours);
+console.log(properties);
+
+let openStr = `We are open on ${properties.length} days `;
+
+for (const day of properties) {
+  openStr += `${day}, `;
+}
+
+console.log(openStr);
+
+//Property values
+const values = Object.values(openingHours);
+console.log(values);
+
+//Entire object
+const entries = Object.entries(openingHours);
+// console.log(entries);
+
+for (const [key, { open, close }] of entries) {
+  console.log(`On ${key} we open at ${open} and close at ${close}`);
+}
+/*
 //////////////////
 //OPTIONAL CHAINING
 if (restaurant.openingHours && restaurant.openingHours.mon)
@@ -78,6 +102,7 @@ console.log(restaurant.orderRisotto?.(0, 1) ?? "Method does not exist");
 //Arrays
 const users = [{ name: "jonas", email: "hello@xxx.com" }];
 console.log(users[0]?.name ?? "User array empty");
+*/
 
 // #1 Coding Challenge
 /* 
