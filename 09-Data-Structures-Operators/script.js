8b0900afba7efcb107e19789d5d1ddd1d1836ff2
sp863 <sp863429@gmail.com>
2021-12-13 03:00:47 +0900

    js09-120 : coding challeng 3

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -53,7 +53,46 @@ const restaurant = {
     console.log(otherIngredients);
   },
 };
+//////////////////////
+// Coding Challenge #3
+const gameEvents = new Map([
+  [17, "⚽️ GOAL"],
+  [36, "🔁 Substitution"],
+  [47, "⚽️ GOAL"],
+  [61, "🔁 Substitution"],
+  [64, "🔶 Yellow card"],
+  [69, "🔴 Red card"],
+  [70, "🔁 Substitution"],
+  [72, "🔁 Substitution"],
+  [76, "⚽️ GOAL"],
+  [80, "⚽️ GOAL"],
+  [92, "🔶 Yellow card"],
+]);
 
+//1.
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+
+//2.
+gameEvents.delete(64);
+console.log(gameEvents);
+
+//3.
+console.log(
+  `An event happened, on average, every ${90 / gameEvents.size} minutes`
+);
+const time = [...gameEvents.keys()].pop();
+console.log(time);
+console.log(
+  `An event happened, on average, every ${time / gameEvents.size} minutes`
+);
+//4.
+for (const [time, event] of gameEvents) {
+  let message = "";
+  time <= 45 ? (message += "[FIRST HALF]") : (message += "[SECOND HALF]");
+  console.log(message + ` ${time}: ${event}`);
+}
+/*
 //MAP - Fundamentals
 const rest = new Map();
 rest.set("name", "Classico Italiano");
@@ -123,6 +162,7 @@ console.log([...question]);
 // console.log(question.entries());
 console.log([question.keys()]);
 console.log([question.values()]);
+*/
 
 /*
 ////////////////////////////////////////////
