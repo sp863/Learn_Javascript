8dbcb31d160b5aba323ede49ed09c1ebd37d25d4
sp863 <sp863429@gmail.com>
2021-12-13 02:24:38 +0900

    js09-118 : maps

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -54,32 +54,77 @@ const restaurant = {
   },
 };
 
+//MAP - Fundamentals
+const rest = new Map();
+rest.set("name", "Classico Italiano");
+rest.set(1, "Firenze");
+rest.set(2, "Lisbon");
+console.log(rest);
+
+rest
+  .set("categories", ["Italian", "Pizzeria", "Vegetarian", "Organic"])
+  .set("open", 11)
+  .set("close", 23)
+  .set(true, "We are open")
+  .set(false, "We are closed");
+
+console.log(rest.get("name"));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 21;
+console.log(rest.get(time > rest.get("open") && time < rest.get("close")));
+
+//check if may has certain key
+console.log(rest.has("categories"));
+rest.delete(2);
+
+const arr = [1, 2];
+rest.set(arr, "Test");
+
+//Objects as map keys like DOM elements -> they are objects
+rest.set(document.querySelector("h1"), "Heading");
+console.log(rest);
+console.log(rest.size);
+
+console.log(rest.get(arr));
+
+//MAP - Iteation
+const question = new Map([
+  ["Question", "What is teh best programming language in the world?"],
+  [1, "C"],
+  [2, "Java"],
+  [3, "JavaScript"],
+  ["correct", 3],
+  [true, "Correct"],
+  [false, "Try again!"],
+]);
+
+console.log(question);
+
+//Convert object to map
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+//Quiz app
+console.log(question.get("question"));
+for (const [key, value] of question) {
+  if (typeof key === "number") console.log(`Answer ${key}: ${value}`);
+}
+// const answer = Number(prompt("Your answer"));
+const answer = 3;
+console.log(answer);
+
+console.log(question.get(question.get("correct") === answer));
+
+//Convert map to array
+console.log([...question]);
+// console.log(question.entries());
+console.log([question.keys()]);
+console.log([question.values()]);
+
 /*
-//Property names
-const properties = Object.keys(openingHours);
-console.log(properties);
-
-let openStr = `We are open on ${properties.length} days `;
-
-for (const day of properties) {
-  openStr += `${day}, `;
-}
-
-console.log(openStr);
-
-//Property values
-const values = Object.values(openingHours);
-console.log(values);
-
-//Entire object
-const entries = Object.entries(openingHours);
-// console.log(entries);
-
-for (const [key, { open, close }] of entries) {
-  console.log(`On ${key} we open at ${open} and close at ${close}`);
-}
-*/
-
 ////////////////////////////////////////////
 //SETS
 
@@ -114,6 +159,33 @@ console.log(
 );
 
 console.log(new Set("sejinpark").size);
+*/
+
+/*
+//Property names
+const properties = Object.keys(openingHours);
+console.log(properties);
+
+let openStr = `We are open on ${properties.length} days `;
+
+for (const day of properties) {
+  openStr += `${day}, `;
+}
+
+console.log(openStr);
+
+//Property values
+const values = Object.values(openingHours);
+console.log(values);
+
+//Entire object
+const entries = Object.entries(openingHours);
+// console.log(entries);
+
+for (const [key, { open, close }] of entries) {
+  console.log(`On ${key} we open at ${open} and close at ${close}`);
+}
+*/
 
 /*
 //Coding Challenge #2
