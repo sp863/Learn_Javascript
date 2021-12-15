5f88af471c95807bf2826c5daaeb3f0a4074b231
sp863 <sp863429@gmail.com>
2021-12-16 04:10:27 +0900

    js12-171 : math

--- 12-Numbers-Dates-Timers-Bankist/script.js
+++ 12-Numbers-Dates-Timers-Bankist/script.js
@@ -81,6 +81,7 @@ console.log(23 === 23.0);
 console.log(0.1 + 0.2);
 console.log(0.1 + 0.2 === 0.3); // false
 
+/*
 // Conversion
 console.log(Number("23"));
 console.log(+"23"); // executes type coercion
@@ -107,3 +108,44 @@ console.log(Number.isFinite(23 / 0));
 console.log(Number.isInteger(23));
 console.log(Number.isInteger(23.0));
 console.log(Number.isInteger(23 / 0));
+
+*/
+
+console.log(Math.sqrt(25));
+console.log(25 ** (1 / 2));
+console.log(8 ** (1 / 3));
+console.log(Math.max(5, 18, 23, 11, 2));
+console.log(Math.max(5, 18, "23", 11, 2));
+console.log(Math.max(5, 18, "23px", 11, 2)); //parsing doesn't work
+console.log(Math.min(5, 18, 23, 11, 2));
+
+console.log(Math.PI * Number.parseFloat("10px") ** 2);
+
+console.log(Math.trunc(Math.random() * 6) + 1);
+
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min) + 1) + min;
+
+console.log(randomInt(10, 20));
+
+// Rounding integers
+console.log(Math.round(23.3));
+console.log(Math.round(23.9));
+
+console.log(Math.ceil(23.3));
+console.log(Math.ceil(23.9));
+
+console.log(Math.floor(23.3));
+console.log(Math.floor("23.9"));
+
+console.log(Math.trunc(23.3));
+
+// floor works with all negative and positive numbers
+console.log(Math.trunc(-23.3));
+console.log(Math.floor(-23.3));
+
+// Rounding decimals
+console.log((2.7).toFixed(0)); // returns a string
+console.log((2.7).toFixed(3)); // returns a string
+console.log((2.345).toFixed(2)); // returns a string
+console.log(+(2.345).toFixed(2)); // returns a string
