5376e95b9b5f4e484a037c3f7d8f02b0a47bc36b
sp863 <sp863429@gmail.com>
2021-12-16 21:02:27 +0900

    js12-175 : date operations

--- 12-Numbers-Dates-Timers-Bankist/script.js
+++ 12-Numbers-Dates-Timers-Bankist/script.js
@@ -216,8 +216,8 @@ console.log(2n / 3n);
 console.log(10 / 3);
 */
 
+/*
 //Date
-
 // Create Date
 const now = new Date();
 console.log(now);
@@ -253,3 +253,13 @@ console.log(Date.now()); //gives time stamp
 
 future.setFullYear(2040);
 console.log(future);
+*/
+
+const future = new Date(2037, 10, 19, 15, 23);
+console.log(+future);
+
+const calcDaysPassed = (date1, date2) =>
+  Math.abs((date2 - date1) / (1000 * 60 * 60 * 24));
+
+const days1 = calcDaysPassed(new Date(2037, 3, 4), new Date(2037, 3, 14));
+console.log(days1);
