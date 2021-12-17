edaa1316cc8c05b37785461f2d828bcc935d855c
sp863 <sp863429@gmail.com>
2021-12-18 02:37:57 +0900

    js14-211 : prototypal inheritance

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -45,3 +45,24 @@ console.log(jonas.species, matilda.species);
 
 console.log(jonas.hasOwnProperty("firstName"));
 console.log(jonas.hasOwnProperty("species")); // -> false
+
+console.log(jonas.__proto__);
+console.log(jonas.__proto__.__proto__); // -> Object.prototype
+console.log(jonas.__proto__.__proto__.__proto__); // -> null
+
+console.log(Person.prototype.constructor); // -> points to Person
+
+const arr = [3, 4, 5, 6, 6, 7, 7];
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype); // -> true
+
+console.log(arr.__proto__.__proto__);
+
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+
+console.log(arr.unique());
+
+const h1 = document.querySelector("h1");
+console.dir((x) => x + 1);
