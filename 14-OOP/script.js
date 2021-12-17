d33bfd3929ca3bd1bdbd7407519cc01f39e80182
sp863 <sp863429@gmail.com>
2021-12-18 03:14:55 +0900

    js14-213 : ES6 classes

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -1,5 +1,5 @@
 "use strict";
-
+/*
 const Person = function (firstName, birthYear) {
   // Instance properties
   this.firstName = firstName;
@@ -91,3 +91,36 @@ bmw.accel();
 bmw.brake();
 benz.accel();
 benz.brake();
+*/
+
+// class expression
+// const PersonCl = class{}
+
+// class declaration
+class PersonCl {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+
+  // Methods will be added to .prototype property
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+
+  greet() {
+    console.log(`Hey ${this.firstName}`);
+  }
+}
+
+const jessica = new PersonCl("Jessica", 1996);
+
+console.log(jessica);
+jessica.calcAge();
+
+console.log(jessica.__proto__ === PersonCl.prototype); // -> true
+
+// PersonCl.prototype.greet = function () {
+//   console.log(`Hey ${this.firstName}`);
+// };
+jessica.greet();
