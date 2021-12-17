c1c07fac3d624cd85c6b97f8655336ff4981b0aa
sp863 <sp863429@gmail.com>
2021-12-18 02:16:32 +0900

    js14-210 : prototype

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -23,3 +23,25 @@ const jay = "Jay";
 
 console.log(jonas instanceof Person);
 console.log(jay instanceof Person);
+
+// Prototypes
+console.log(Person.prototype);
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+jonas.calcAge(); //doesn't have the calcAge method but because of prototype, jonas can use this method
+matilda.calcAge();
+
+console.log(jonas.__proto__); // -> prototype of jonas created by step 3 form above
+console.log(jonas.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(jonas));
+console.log(Person.prototype.isPrototypeOf(matilda));
+console.log(Person.prototype.isPrototypeOf(Person)); // -> this is false!
+
+Person.prototype.species = "Homo Sapiens";
+console.log(jonas.species, matilda.species);
+
+console.log(jonas.hasOwnProperty("firstName"));
+console.log(jonas.hasOwnProperty("species")); // -> false
