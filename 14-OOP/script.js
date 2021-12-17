15230dc83fc6a58f82878d5bedd6eb1f65184895
sp863 <sp863429@gmail.com>
2021-12-18 05:19:19 +0900

    js14-221 : using object create

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -299,7 +299,7 @@ const tesla = new EV("Tesla", 120, 23);
 tesla.chargeBattery(90);
 tesla.accel();
 */
-
+/*
 class PersonCl {
   constructor(fullName, birthYear) {
     this.fullName = fullName;
@@ -360,3 +360,30 @@ class StudentCl extends PersonCl {
 const martha = new StudentCl("Martha Jones", 2012, "Computer Science");
 martha.introduce();
 martha.calcAge();
+*/
+
+const PersonProto = {
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  },
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+
+const StudentProto = Object.create(PersonProto);
+
+StudentProto.init = function (firstName, birthYear, course) {
+  PersonProto.init.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+StudentProto.introduce = function () {
+  console.log(`My name is ${this.firstName} and I study ${this.course}`);
+};
+
+const jay = Object.create(StudentProto);
+jay.init("Jay", 2010, "Computer Science");
+jay.introduce();
+jay.calcAge();
