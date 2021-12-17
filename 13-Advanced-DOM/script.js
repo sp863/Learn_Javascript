0697b2f45ac029322a1a0a328c889aad7b038830
sp863 <sp863429@gmail.com>
2021-12-17 15:01:18 +0900

    js13-193 : dom traversing

--- 13-Advanced-DOM/script.js
+++ 13-Advanced-DOM/script.js
@@ -104,6 +104,7 @@ setTimeout(() => h1.removeEventListener("mouseenter", alertH1), 3000);
 // };
 */
 
+/*
 // rgb(255,255,255)
 const randomInt = (min, max) =>
   Math.floor(Math.random() * (max - min + 1) + min);
@@ -129,3 +130,33 @@ document.querySelector(".nav").addEventListener(
   },
   false
 );
+*/
+
+const h1 = document.querySelector("h1");
+
+// Goind downwards: child
+console.log(h1.querySelectorAll(".highlight")); // children of the h1 element
+console.log(h1.childNodes);
+console.log(h1.children);
+
+h1.firstElementChild.style.color = "white";
+h1.lastElementChild.style.color = "orangered";
+
+// Going upwards: parents
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+
+h1.closest(".header").style.background = "var(--gradient-secondary)";
+h1.closest("h1").style.background = "var(--gradient-secondary)";
+
+// Going sideways: siblings
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+
+console.log(h1.previousSibling);
+console.log(h1.nextSibling);
+
+console.log(h1.parentElement.children);
+[...h1.parentElement.children].forEach(function (el) {
+  if (el !== h1) el.style.transform = "scale(0.5";
+});
