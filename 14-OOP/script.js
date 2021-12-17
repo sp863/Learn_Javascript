bfa16e00b42dbe877a08389393755664b8351da4
sp863 <sp863429@gmail.com>
2021-12-18 04:53:28 +0900

    js14-219 : coding challenge 3

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -206,7 +206,7 @@ const sarah = Object.create(PersonProto);
 sarah.init("Sarah", 1978);
 sarah.calcAge();
 */
-
+/*
 // coding challenge #2
 class Car {
   constructor(make, speed) {
@@ -224,3 +224,75 @@ class Car {
 }
 
 const ford = new Car("Ford", 120);
+*/
+const Person = function (firstName, birthYear) {
+  // Instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+const Student = function (firstName, birthYear, course) {
+  Person.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+Student.prototype = Object.create(Person.prototype);
+
+Student.prototype.introduce = function () {
+  console.log(`My name is ${this.firstName} and I study ${this.course}`);
+};
+const mike = new Student("Mike", 2020, "Computer Science");
+mike.introduce();
+mike.calcAge();
+
+console.log(mike.__proto__);
+console.log(mike.__proto__.__proto__);
+
+console.log(mike instanceof Student); // -> true
+console.log(mike instanceof Person); // -> true
+console.log(mike instanceof Object); // -> true
+
+Student.prototype.constructor = Student;
+console.log(Student.prototype.constructor);
+
+// coding challenge #3
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accel = function () {
+  this.speed += 10;
+  console.log(this.speed);
+};
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(this.speed);
+};
+
+const EV = function (make, speed, charge) {
+  Car.call(this, make, speed);
+  this.charge = charge;
+};
+
+EV.prototype = Object.create(Car.prototype);
+
+EV.prototype.chargeBattery = function (chargeTo) {
+  this.charge = chargeTo;
+};
+
+EV.prototype.accel = function () {
+  this.speed += 20;
+  this.charge--;
+  console.log(
+    `${this.make} going at ${this.speed} km/h, with a charge of ${this.charge}`
+  );
+};
+
+const tesla = new EV("Tesla", 120, 23);
+tesla.chargeBattery(90);
+tesla.accel();
